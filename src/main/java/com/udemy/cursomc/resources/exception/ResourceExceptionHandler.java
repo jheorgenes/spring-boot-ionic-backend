d519d908d69d14c67ead09476f27b342f79f85e1
Jheorgenes Warlley <jheorgenes@gmail.com>
2022-01-27 16:16:29 -0300

    Personalizando o retorno da validação

--- src/main/java/com/udemy/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/udemy/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -3,6 +3,8 @@ package com.udemy.cursomc.resources.exception;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,10 +21,23 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
 	
-	@ExceptionHandler(DataIntegrityException.class) //Anotation para definir que esse método é um tratador de excessão do tipo de exception passada por parametro
+	@ExceptionHandler(DataIntegrityException.class) 
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 		
 		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class) 
+	public ResponseEntity<StandardError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+		
+		ValidationError error = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+		
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			error.addError(x.getField(), x.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
 }
--- src/main/java/com/udemy/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/udemy/cursomc/resources/exception/StandardError.java
@@ -2,7 +2,7 @@ package com.udemy.cursomc.resources.exception;
 
 import java.io.Serializable;
 
-public class StandardError implements Serializable{
+public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer status;
